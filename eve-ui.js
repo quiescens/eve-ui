4939928ebc53d83938989cae996aa95d0c3f5fe2
quiescens <quiescens@gmail.com>
2016-09-20 08:21:14 +0000

    added the ability to specify that a fit should be expanded in place, on document load added the option to expand all fits in place on document load added example to demonstrate a fit expanded in place

--- eve-ui.js
+++ eve-ui.js
@@ -3,6 +3,7 @@
 // config stuff ( can be overridden in a script block placed immediately after the script tag for this script )
 var eveui_preload_initial = 50;
 var eveui_preload_interval = 10;
+var eveui_expand_all = false;
 var eveui_allow_edit = false;
 var eveui_fit_selector = "[href^=fitting],[data-dna]";
 var eveui_item_selector = "[href^=item],[data-itemid]";
@@ -11,22 +12,22 @@ function eveui_autocomplete_endpoint(str) { return 'https://zkillboard.com/autoc
 var eveui_style = `
     <style>
         .eveui_window { font-family: sans-serif; line-height: 1; background: #eee; border: 1px solid; opacity: 0.95; min-width: 150px; min-height: 100px; }
-        .eveui_window table { border-spacing: 0; }
-        .eveui_window td { padding: 0; }
-        .eveui_window th { line-height: 0.5; }
-        .eveui_window .eveui_title { position: absolute; top: 0; left: 0; width: 100%; background: #ccc; cursor: move; }
-        .eveui_window .eveui_scrollable { padding-right: 17px; text-align: left; overflow: auto; display: inline-block; max-width: 100%; max-height: 100%; }
-        .eveui_window .eveui_content { display: table; margin-top: 15px; padding: 5px; white-space: nowrap; }
-        .eveui_window .eveui_item_icon { display: inline-block; vertical-align: middle; margin: 1px 2px; background-size: 20px 20px; height: 20px; width: 20px; }
-        .eveui_window .eveui_ship_icon { display: inline-block; vertical-align: middle; margin: 1px 2px; background-size: 32px 32px; height: 32px; width: 32px; }
-        .eveui_window .eveui_info_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI4OTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTQ0OCAzODRjMzUgMCA2NC0yOSA2NC02NHMtMjktNjQtNjQtNjQtNjQgMjktNjQgNjQgMjkgNjQgNjQgNjR6IG0wLTMyMGMtMjQ3IDAtNDQ4IDIwMS00NDggNDQ4czIwMSA0NDggNDQ4IDQ0OCA0NDgtMjAxIDQ0OC00NDgtMjAxLTQ0OC00NDgtNDQ4eiBtMCA3NjhjLTE3NyAwLTMyMC0xNDMtMzIwLTMyMHMxNDMtMzIwIDMyMC0zMjAgMzIwIDE0MyAzMjAgMzIwLTE0MyAzMjAtMzIwIDMyMHogbTY0LTMyMGMwLTMyLTMyLTY0LTY0LTY0cy0zMiAwLTY0IDAtNjQgMzItNjQgNjRoNjRzMCAxNjAgMCAxOTIgMzIgNjQgNjQgNjQgMzIgMCA2NCAwIDY0LTMyIDY0LTY0aC02NHMwLTE2MCAwLTE5MnoiIC8+Cjwvc3ZnPgo=); }
-        .eveui_window .eveui_plus_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI2NDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTM4NCA0NDhWMTkySDI1NnYyNTZIMHYxMjhoMjU2djI1NmgxMjhWNTc2aDI1NlY0NDhIMzg0eiIgLz4KPC9zdmc+Cg==); }
-        .eveui_window .eveui_minus_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTAgNDQ4djEyOGg1MTJWNDQ4SDB6IiAvPgo8L3N2Zz4K); }
-        .eveui_window .eveui_more_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI3NjgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTAgNTc2aDEyOHYtMTI4aC0xMjh2MTI4eiBtMC0yNTZoMTI4di0xMjhoLTEyOHYxMjh6IG0wIDUxMmgxMjh2LTEyOGgtMTI4djEyOHogbTI1Ni0yNTZoNTEydi0xMjhoLTUxMnYxMjh6IG0wLTI1Nmg1MTJ2LTEyOGgtNTEydjEyOHogbTAgNTEyaDUxMnYtMTI4aC01MTJ2MTI4eiIgLz4KPC9zdmc+Cg==); }
-        .eveui_window .eveui_edit_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI4OTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTcwNCA2NEw1NzYgMTkybDE5MiAxOTIgMTI4LTEyOEw3MDQgNjR6TTAgNzY4bDAuNjg4IDE5Mi41NjJMMTkyIDk2MGw1MTItNTEyTDUxMiAyNTYgMCA3Njh6TTE5MiA4OTZINjRWNzY4aDY0djY0aDY0Vjg5NnoiIC8+Cjwvc3ZnPgo=); }
-        .eveui_window .close_button { position: absolute; top: 0; right: 5px; cursor: pointer; height: 1em; width: 1em; background-size: 1em 1em; background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHg9IjciIHk9Ii0xIiB0cmFuc2Zvcm09Im1hdHJpeCgwLjcwNzEgLTAuNzA3MSAwLjcwNzEgMC43MDcxIC0zLjMxMzUgNy45OTk1KSIgd2lkdGg9IjIiIGhlaWdodD0iMTcuOTk5Ii8+CjxyZWN0IHg9IjciIHk9Ii0wLjk5OSIgdHJhbnNmb3JtPSJtYXRyaXgoLTAuNzA3MSAtMC43MDcxIDAuNzA3MSAtMC43MDcxIDcuOTk4OCAxOS4zMTQyKSIgd2lkdGg9IjIiIGhlaWdodD0iMTcuOTk5Ii8+PC9zdmc+Cg==); }
-        .eveui_window .copy_only { position: absolute; display:inline-block; line-height: 0; font-size: 0; }
-        .eveui_window .nocopy::after { content: attr(data-content); }
+        .eveui_fit_table { border-spacing: 0; }
+        .eveui_fit_table td { padding: 0; }
+        .eveui_fit_table th { line-height: 0.5; }
+        .eveui_title { position: absolute; top: 0; left: 0; width: 100%; background: #ccc; cursor: move; }
+        .eveui_scrollable { padding-right: 17px; text-align: left; overflow: auto; display: inline-block; max-width: 100%; max-height: 100%; }
+        .eveui_content { display: table; margin-top: 15px; padding: 5px; white-space: nowrap; }
+        .eveui_item_icon { display: inline-block; vertical-align: middle; margin: 1px 2px; background-size: 20px 20px; height: 20px; width: 20px; }
+        .eveui_ship_icon { display: inline-block; vertical-align: middle; margin: 1px 2px; background-size: 32px 32px; height: 32px; width: 32px; }
+        .eveui_info_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI4OTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTQ0OCAzODRjMzUgMCA2NC0yOSA2NC02NHMtMjktNjQtNjQtNjQtNjQgMjktNjQgNjQgMjkgNjQgNjQgNjR6IG0wLTMyMGMtMjQ3IDAtNDQ4IDIwMS00NDggNDQ4czIwMSA0NDggNDQ4IDQ0OCA0NDgtMjAxIDQ0OC00NDgtMjAxLTQ0OC00NDgtNDQ4eiBtMCA3NjhjLTE3NyAwLTMyMC0xNDMtMzIwLTMyMHMxNDMtMzIwIDMyMC0zMjAgMzIwIDE0MyAzMjAgMzIwLTE0MyAzMjAtMzIwIDMyMHogbTY0LTMyMGMwLTMyLTMyLTY0LTY0LTY0cy0zMiAwLTY0IDAtNjQgMzItNjQgNjRoNjRzMCAxNjAgMCAxOTIgMzIgNjQgNjQgNjQgMzIgMCA2NCAwIDY0LTMyIDY0LTY0aC02NHMwLTE2MCAwLTE5MnoiIC8+Cjwvc3ZnPgo=); }
+        .eveui_plus_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI2NDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTM4NCA0NDhWMTkySDI1NnYyNTZIMHYxMjhoMjU2djI1NmgxMjhWNTc2aDI1NlY0NDhIMzg0eiIgLz4KPC9zdmc+Cg==); }
+        .eveui_minus_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI1MTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTAgNDQ4djEyOGg1MTJWNDQ4SDB6IiAvPgo8L3N2Zz4K); }
+        .eveui_more_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI3NjgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTAgNTc2aDEyOHYtMTI4aC0xMjh2MTI4eiBtMC0yNTZoMTI4di0xMjhoLTEyOHYxMjh6IG0wIDUxMmgxMjh2LTEyOGgtMTI4djEyOHogbTI1Ni0yNTZoNTEydi0xMjhoLTUxMnYxMjh6IG0wLTI1Nmg1MTJ2LTEyOGgtNTEydjEyOHogbTAgNTEyaDUxMnYtMTI4aC01MTJ2MTI4eiIgLz4KPC9zdmc+Cg==); }
+        .eveui_edit_icon { display: inline-block; margin: 0 1px; height: 1em; width: 1em; background-position: center; background-repeat: no-repeat; background-size: contain; background-image: url(data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjEwMjQiIHdpZHRoPSI4OTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTTcwNCA2NEw1NzYgMTkybDE5MiAxOTIgMTI4LTEyOEw3MDQgNjR6TTAgNzY4bDAuNjg4IDE5Mi41NjJMMTkyIDk2MGw1MTItNTEyTDUxMiAyNTYgMCA3Njh6TTE5MiA4OTZINjRWNzY4aDY0djY0aDY0Vjg5NnoiIC8+Cjwvc3ZnPgo=); }
+        .close_button { position: absolute; top: 0; right: 5px; cursor: pointer; height: 1em; width: 1em; background-size: 1em 1em; background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxyZWN0IHg9IjciIHk9Ii0xIiB0cmFuc2Zvcm09Im1hdHJpeCgwLjcwNzEgLTAuNzA3MSAwLjcwNzEgMC43MDcxIC0zLjMxMzUgNy45OTk1KSIgd2lkdGg9IjIiIGhlaWdodD0iMTcuOTk5Ii8+CjxyZWN0IHg9IjciIHk9Ii0wLjk5OSIgdHJhbnNmb3JtPSJtYXRyaXgoLTAuNzA3MSAtMC43MDcxIDAuNzA3MSAtMC43MDcxIDcuOTk4OCAxOS4zMTQyKSIgd2lkdGg9IjIiIGhlaWdodD0iMTcuOTk5Ii8+PC9zdmc+Cg==); }
+        .copy_only { position: absolute; display:inline-block; line-height: 0; font-size: 0; }
+        .nocopy::after { content: attr(data-content); }
     </style>
     `;
 
@@ -71,15 +72,8 @@ $( document ).ready( function() {
         this.eveui_window.attr( "data-eveui-dna", dna );
         $( "body" ).append( this.eveui_window );
         
-        eveui_mark( "fit window created" );
+        eveui_fit_window( dna, eveui_name );
 
-        // load required items and set callback to display
-        eveui_cache_fit( dna ).done( function() {
-            eveui_fit_show( dna, eveui_name );
-            eveui_mark( "fit window populated" );
-        }).fail( function() {
-            delete eveui_fit_cache[ dna ];
-        });
     });
 
     $( document ).on( "click", eveui_item_selector, function(e) {
@@ -138,7 +132,7 @@ $( document ).ready( function() {
         }
 
         $( this ).closest( "[data-eveui-dna]" ).attr( "data-eveui-dna", dna );
-        eveui_fit_show( dna );
+        eveui_fit_window( dna );
     });
 
     $( document ).on( "click", ".eveui_plus_icon", function(e) {
@@ -155,7 +149,7 @@ $( document ).ready( function() {
         }
 
         $( this ).closest( "[data-eveui-dna]" ).attr( "data-eveui-dna", dna );
-        eveui_fit_show( dna );
+        eveui_fit_window( dna );
     });
 
     $( document ).on( "click", ".eveui_more_icon", function(e) {
@@ -213,6 +207,8 @@ $( document ).ready( function() {
     eveui_preload_timer = setTimeout( eveui_lazy_preload, 1000 );
 
     eveui_mark( "document ready end" );
+
+    eveui_expand_fits();
 });
 
 function eveui_newwindow() {
@@ -230,7 +226,7 @@ function eveui_mark( mark ) {
     }
 }
 
-function eveui_fit_show( dna, eveui_name ) {
+function eveui_generate_fit( dna, eveui_name ) {
     // generates and displays a fit
     var high_slots = {};
     var med_slots = {};
@@ -326,7 +322,7 @@ function eveui_fit_show( dna, eveui_name ) {
     }
 
     var html = '';
-    html += '<table>';
+    html += '<table class="eveui_fit_table">';
     html += '<tr data-eveui-itemid=' + ship_id + '>';
     html += '<td><span style="background-image: url(https://imageserver.eveonline.com/Type/' + ship_id + '_32.png)" class="eveui_ship_icon" />';
     html += '[' + eveui_item_cache[ ship_id ].name + ', <a target="_blank" href="' + eveui_urlify( dna ) + '">' + ( eveui_name || eveui_item_cache[ ship_id ].name ) + '</a>]';
@@ -347,10 +343,39 @@ function eveui_fit_show( dna, eveui_name ) {
     html += item_rows( other_slots );
     html += '</table>';
 
-    var eveui_window = $( '.eveui_window[data-eveui-dna="' + dna + '"]' );
-    eveui_window.find(".eveui_content").html( html );
+    return html;
+}
 
-    $( window ).trigger( "resize" );
+function eveui_fit_window( dna, eveui_name ) {
+    // load required items and set callback to display
+    eveui_mark( "fit window created" );
+    eveui_cache_fit( dna ).done( function() {
+        var eveui_window = $( '.eveui_window[data-eveui-dna="' + dna + '"]' );
+        eveui_window.find(".eveui_content").html( eveui_generate_fit( dna, eveui_name) );
+
+        $( window ).trigger( "resize" );
+        eveui_mark( "fit window populated" );
+    }).fail( function() {
+        delete eveui_fit_cache[ dna ];
+    });
+}
+
+function eveui_expand_fits() {
+    var expand_filter = "[data-eveui-expand]";
+    if ( eveui_expand_all ) {
+        expand_filter = "*";
+    }
+    $( eveui_fit_selector ).filter( expand_filter ).each( function() {
+        var dna = $(this).attr("data-dna") || this.href.substring(this.href.indexOf(":") + 1);
+        var selected_element = $( this );
+        eveui_cache_fit( dna ).done( function() {
+            eveui_mark( "fit window populated" );
+            var eveui_name = $(this).text();
+            selected_element.replaceWith( eveui_generate_fit( dna ) );
+        }).fail( function() {
+            delete eveui_fit_cache[ dna ];
+        });
+    });
 }
 
 function eveui_lazy_preload() {
