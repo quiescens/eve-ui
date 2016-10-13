2ae8878f63df384b83b9f5e22d6d06aa2027b9d2
quiescens <quiescens@gmail.com>
2016-10-13 18:38:10 +0000

    fixed bug in es6 definition

--- lib/es6.d.ts
+++ lib/es6.d.ts
@@ -1,5 +1,5 @@
 interface String {
-    startsWith(str: string, pos: number?): boolean
+    startsWith(str: string, pos?: number): boolean
     repeat(count: number): string
     substr(from: number, length?: number): string
 }
