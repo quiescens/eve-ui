4e103ef9a2ea5befbd85bf53cf782ff640ab378a
quiescens <quiescens@gmail.com>
2017-07-12 07:01:49 +0000

    Removed internal browser compatibility shims
    If you still need compatibility for particularly old browsers
    transcoding and JS shims are being left for the user.

--- src/eve-ui.ts
+++ src/eve-ui.ts
@@ -1123,58 +1123,5 @@ namespace eveui {
 		$( '.copyonly' ).hide();
 	}
 
-	// additional shims for older browsers
-	/* from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/repeat */
-	if (!String.prototype.repeat) {
-		String.prototype.repeat = function(count) {
-		'use strict';
-		if (this == null) {
-			throw new TypeError('can\'t convert ' + this + ' to object');
-		}
-		let str = '' + this;
-		count = +count;
-		if (count != count) {
-			count = 0;
-		}
-		if (count < 0) {
-			throw new RangeError('repeat count must be non-negative');
-		}
-		if (count == Infinity) {
-			throw new RangeError('repeat count must be less than infinity');
-		}
-		count = Math.floor(count);
-		if (str.length == 0 || count == 0) {
-			return '';
-		}
-		// Ensuring count is a 31-bit integer allows us to heavily optimize the
-		// main part. But anyway, most current (August 2014) browsers can't handle
-		// strings 1 << 28 chars or longer, so:
-		if (str.length * count >= 1 << 28) {
-			throw new RangeError('repeat count must not overflow maximum string size');
-		}
-		let rpt = '';
-		for (;;) {
-			if ((count & 1) == 1) {
-				rpt += str;
-			}
-			count >>>= 1;
-			if (count == 0) {
-				break;
-			}
-			str += str;
-		}
-		// Could we try:
-		// return Array(count + 1).join(this);
-		return rpt;
-		}
-	}
-	/* https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith */
-	if (!String.prototype.startsWith) {
-		String.prototype.startsWith = function(searchString, position){
-			position = position || 0;
-			return this.substr(position, searchString.length) === searchString;
-		};
-	}
-
 	mark( 'script end' );
 }
