da3f8c0692fa1f129090dab9a6a8b306891f70ce
quiescens <quiescens@gmail.com>
2017-07-12 09:17:21 +0000

    0.8.4

--- eve-ui.js
+++ eve-ui.js
@@ -5,7 +5,7 @@
 // ` used whenever interpolation is required
 'use strict';
 // config stuff ( can be overridden in a script block or js file of your choice )
-var eveui_user_agent = eveui_user_agent || 'For source website, see referrer. For library, see https://github.com/quiescens/eve-ui/';
+var eveui_user_agent = eveui_user_agent || 'For source website, see referrer. For library, see https://github.com/quiescens/eve-ui/ r:' + `87680b0`;
 var eveui_use_localstorage = eveui_use_localstorage || 4000000;
 var eveui_preload_initial = eveui_preload_initial || 50;
 var eveui_preload_interval = eveui_preload_interval || 10;
